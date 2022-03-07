9638aff88a94f94a1ffd2a26ce5a850d695462a8
Iker Iriondo <iker.i.p.3@gmail.com>
2022-03-07 18:11:17 +0100

    Gertaera eta Kuota

--- src/main/java/gui/GertaeraSortuGUI.java
+++ src/main/java/gui/GertaeraSortuGUI.java
@@ -1,11 +1,17 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Frame;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
 
-public class GertaeraSortuGUI {
+public class GertaeraSortuGUI extends Frame{
 
 	private JFrame frame;
 
@@ -29,7 +35,22 @@ public class GertaeraSortuGUI {
 	 * Create the application.
 	 */
 	public GertaeraSortuGUI() {
+		super();
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				try {
+					//if (ConfigXML.getInstance().isBusinessLogicLocal()) facade.close();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					System.out.println("Error: "+e1.toString()+" , probably problems with Business Logic or Database");
+				}
+				System.exit(1);
+			}
+		});
+		
 		initialize();
+		frame.setVisible(true);
 	}
 
 	/**
@@ -44,5 +65,15 @@ public class GertaeraSortuGUI {
 		JLabel lblNewLabel = new JLabel("Kaixo");
 		lblNewLabel.setBounds(159, 69, 45, 13);
 		frame.getContentPane().add(lblNewLabel);
+		
+		JButton atzeraButton = new JButton("Atzera");
+		atzeraButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				AdminGUI a = new AdminGUI();
+			}
+		});
+		atzeraButton.setBounds(129, 139, 89, 23);
+		frame.getContentPane().add(atzeraButton);
 	}
 }
--- src/main/java/gui/KuotaIpiniGUI.java
+++ src/main/java/gui/KuotaIpiniGUI.java
@@ -1,10 +1,16 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Frame;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
 
-public class KuotaIpiniGUI {
+public class KuotaIpiniGUI extends Frame{
 
 	private JFrame frame;
 
@@ -28,7 +34,21 @@ public class KuotaIpiniGUI {
 	 * Create the application.
 	 */
 	public KuotaIpiniGUI() {
+		super();
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				try {
+					//if (ConfigXML.getInstance().isBusinessLogicLocal()) facade.close();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					System.out.println("Error: "+e1.toString()+" , probably problems with Business Logic or Database");
+				}
+				System.exit(1);
+			}
+		});
 		initialize();
+		frame.setVisible(true);
 	}
 
 	/**
@@ -38,6 +58,16 @@ public class KuotaIpiniGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton atzeraButton = new JButton("Atzera");
+		atzeraButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				AdminGUI a = new AdminGUI();
+			}
+		});
+		atzeraButton.setBounds(176, 109, 89, 23);
+		frame.getContentPane().add(atzeraButton);
 	}
-
 }
