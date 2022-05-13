9e72703005b6d92ee24e6b0806125bb7047f61aa
iByN8 <ibaiheras2002@gmail.com>
2022-05-13 12:34:56 +0200

    Apustu anitza interfazeak

---
+++ src/main/java/gui/ApustuAnitzakGUI.java
@@ -0,0 +1,95 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class ApustuAnitzakGUI {
+
+	private JFrame frame;
+	private JTextField KuotaField;
+	private JTextField DiruaField;
+	private JTable elkarrizketakTable;
+	private DefaultTableModel elkarrizketakTableModel;
+	private String[] elkarrizketakColumnNames = {"Galdera","Erantzun Posiblea"};
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ApustuAnitzakGUI window = new ApustuAnitzakGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ApustuAnitzakGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 558, 367);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel KuotaLabel = new JLabel("New label");
+		KuotaLabel.setBounds(292, 57, 46, 14);
+		frame.getContentPane().add(KuotaLabel);
+		
+		JLabel DiruaLabel = new JLabel("New label");
+		DiruaLabel.setBounds(292, 102, 46, 14);
+		frame.getContentPane().add(DiruaLabel);
+		
+		KuotaField = new JTextField();
+		KuotaField.setBounds(348, 54, 170, 20);
+		frame.getContentPane().add(KuotaField);
+		KuotaField.setColumns(10);
+		
+		DiruaField = new JTextField();
+		DiruaField.setBounds(348, 99, 170, 20);
+		frame.getContentPane().add(DiruaField);
+		DiruaField.setColumns(10);
+		
+		JButton apustuAnitzaEginButton = new JButton("New button");
+		apustuAnitzaEginButton.setBounds(323, 254, 170, 40);
+		frame.getContentPane().add(apustuAnitzaEginButton);
+		
+		JButton apustuaGehituButton = new JButton("New button");
+		apustuaGehituButton.setBounds(351, 190, 111, 33);
+		frame.getContentPane().add(apustuaGehituButton);
+		
+		JScrollPane elkarrizketakScrollPane = new JScrollPane();
+		
+		elkarrizketakScrollPane.setBounds(10, 11, 272, 306);
+		frame.getContentPane().add(elkarrizketakScrollPane);
+		
+		elkarrizketakTable = new JTable();
+		elkarrizketakScrollPane.setViewportView(elkarrizketakTable);
+	
+		elkarrizketakTableModel = new DefaultTableModel(null, elkarrizketakColumnNames);
+		elkarrizketakTable.setModel(elkarrizketakTableModel);	
+		
+		elkarrizketakTable.getColumnModel().getColumn(0).setPreferredWidth(30);
+		elkarrizketakTable.getColumnModel().getColumn(1).setPreferredWidth(200);
+	}
+}
---
+++ src/main/java/gui/ApustuMotaGUI.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class ApustuMotaGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ApustuMotaGUI window = new ApustuMotaGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	 * Create the application.
+	 */
+	
+	public ApustuMotaGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton apustuEginButton = new JButton("New button");
+		apustuEginButton.setBounds(143, 76, 132, 30);
+		frame.getContentPane().add(apustuEginButton);
+		
+		JButton apustuAnitzaEginButton = new JButton("New button");
+		apustuAnitzaEginButton.setBounds(143, 135, 132, 30);
+		frame.getContentPane().add(apustuAnitzaEginButton);
+	}
+
+}
